dfa315dcdc6fa2f5473ecd7ec3612e99106f48fa
Bassil <116284488+Bassil88@users.noreply.github.com>
2023-10-19 11:34:18 +0200

    Update axiosConfig.js

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const setAxiosDefaults = () => {
   process.env.NODE_ENV === "production"
     ? (axios.defaults.baseURL = "https://lanola.onrender.com")
-    : (axios.defaults.baseURL = "http://localhost:8000");
+    : (axios.defaults.baseURL = "https://la-nola-backend.onrender.com");
 
   axios.defaults.withCredentials = true;
 };
